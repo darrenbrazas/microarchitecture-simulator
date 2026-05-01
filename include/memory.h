e4bce93800dbfd92262fa77606974f56e6ae2169
Darren Brazas <darrenbrazas2205@gmail.com>
2026-04-30 23:01:02 -0400

    Simulated memory module

---
+++ include/memory.h
@@ -0,0 +1,23 @@
+#include <cstdint>
+
+struct Memory {
+
+    uint8_t memory[65536] = {0}; // 64KB
+
+
+    int load(uint32_t address){
+
+        return memory[address];
+
+    }
+
+    //void type function so it should not return a data type (void)
+    void store(uint16_t address, int value){
+
+       
+
+            memory[address] = value;
+        
+
+    }
+}
